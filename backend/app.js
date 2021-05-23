74b2f56facd157a018f612d9cf28479e47baf7de
adeniasfilho <adeniasfilho.n1@gmail.com>
2021-05-23 18:58:13 -0300

    Commit inicial

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,41 @@
-import { createConnection } from 'mysql';
-const connection = createConnection();
-const app = express();
-app.use(express.json().urlencoded());
+import app from "../server";
 
-app.post('/login', (req, res) => {
-    const user = req.body.user;
-    const pass = req.body.pass;
-    const sql = "SELECT * FROM users WHERE user = $1 AND pass = $2";
-    deb.query(sql, [user, pass], (err, results) => {
-        connection.query(sql, function (err, results) {});
-    })
-});
+passport.use('local-signup', new LocalStrategy(
+{
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+},
+));
 
-const module = module.exports;
-module.exports = app;
+app.use(session ({
+    secret: 'secret key',
+    resave: true,
+    saveUninitialized: true 
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.post('/login',
+passport.authenticate('local', { successRedirect: '/',
+failureRedirect: '/login',
+failureFlash: true}));
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+        username.findOne({ username: username }, (err, user) => {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                return done(null, false, { message: 'Username incorreto.'});
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, { message: 'Password errada.'});
+            }
+            return done(null, user);
+        })
+    }
+))
